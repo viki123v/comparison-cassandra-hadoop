103aa660a2931cdcfc089704227ea6c6cf1e560d
viki123v <viktorhristovski629@gmail.com>
2026-07-19 11:35:41 +0200

    [FTR] Idempotent loading of data

--- cassandra/src/cassandra_nosql/load_data/__main__.py
+++ cassandra/src/cassandra_nosql/load_data/__main__.py
@@ -3,13 +3,25 @@ from cassandra_nosql import session
 from cassandra_nosql.models.cassndra import KEYSPACE
 from pathlib import Path 
 
-SCHEMA_SQL_PATH = Path(__file__).parent.parent.parent.parent / "schema.cql"
+KEYSPACE_CQL_PATH= Path(__file__).parent.parent.parent.parent / "cql" / "keyspace.cql"
+SCHEMA_CQL_PATH = Path(__file__).parent.parent.parent.parent / "cql" / "schema.cql"
+
+# The session.execute executes ONLY a single statement 
+def execute_cql_file(path: Path):
+    with open(path, "r") as f:
+        statements = f.read().split(";")
+
+    for statement in statements:
+        statement = statement.strip()
+        if statement:
+            session.execute(statement)
+
 
 def clean_database():
     session.execute(f"DROP KEYSPACE IF EXISTS {KEYSPACE}")
-    with open(SCHEMA_SQL_PATH, "r") as f:
-        schema = f.read()
-        session.execute(schema)
+
+    execute_cql_file(KEYSPACE_CQL_PATH)
+    execute_cql_file(SCHEMA_CQL_PATH)
 
 
 def import_data():
